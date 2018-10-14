835c96b59126cbe93c6e02ab24ef16ca7abb623a
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-14 18:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-add(1 + 80) // 81
-subtract(60 - 40) // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
+function add(x + y){
+  x = 1
+  y = 2
+  return add;
+}; // 81
+function subtract(60 - 40){}; // 20
+function multiply(2 * 3.4){}; // 6.8 (there's that floating-point arithmetic again...)
+function divide(5.0 / 2.5){}; // 2
